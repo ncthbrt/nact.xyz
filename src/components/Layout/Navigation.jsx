7deeeb96b81499d619bf8e3acbe4adc87af267d8
Nick Cuthbert <nick@cuthbert.co.za>
2017-12-16 19:58:13 +0200

    Firacode now working in safari

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -15,8 +15,6 @@ const NavContainer = styled.div`
     font-weight: 200;           
     color: ${props => props.isSubpage ? props.theme.accent : props.theme.brand};      
   }  
-
-
   .nav-link:hover {
     border-color:  ${props => props.isSubpage ? props.theme.accent : props.theme.brand};
   }
@@ -33,12 +31,17 @@ class Navigation extends React.Component {
           }
         </section>
         <section style={{animationDelay: '350ms'}} className={this.props.isSubpage ? ' ' : 'animated fadeIn'}>
-          <Link className='nav-link' to='/lesson/javascript/introduction'> <img alt='javascript' style={{height: '1em'}} src={`/logos/language-logo_js${this.props.isSubpage ? '' : '-inverted'}.svg`} /> </Link>
+          {/* <Link className='nav-link' to='/lesson/javascript/introduction'> <img alt='javascript' style={{height: '1em'}} src={`/logos/language-logo_js${this.props.isSubpage ? '' : '-inverted'}.svg`} /> </Link>
           <span className='nav-link' style={{height: '1em'}}>/</span>
           <Link className='nav-link' to='/lesson/reasonml/introduction' > <img alt='reason' style={{height: '1em'}} src={`/logos/language-logo_reason${this.props.isSubpage ? '' : '-inverted'}.svg`} /> </Link>
           <Link className='nav-link' to='/blog' > BLOG </Link>
           <Link className='nav-link' to='/community' > COMMUNITY </Link>
-          <a className='nav-link' href='https://github.com/ncthbrt/nact'> GITHUB </a>
+          <a className='nav-link' href='https://github.com/ncthbrt/nact'> GITHUB </a> */}
+          <div className='open'>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+          </div>
         </section>
       </NavContainer>
     );
