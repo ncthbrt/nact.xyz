ca95c2fd200ee8c4fc4bdefc002be58c3366203f
Nick Cuthbert <nick@cuthbert.co.za>
2017-12-16 21:59:50 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-820c05d0449366e51a0c.js","9a6a22149cdb17264efae80c5d14168c"],["/commons-95cd41fd94d5a626681e.js","7655c6c0cb09cd710f356047c9c76777"],["/index.html","8451be73658b47ceb96e899faf7194f2"],["/manifest.json","2027904c18caa3d9f4569177e8406bea"],["/offline-plugin-app-shell-fallback/index.html","091226639552d63c0fa14c15f55e3638"]];
+var precacheConfig = [["/app-820c05d0449366e51a0c.js","9a6a22149cdb17264efae80c5d14168c"],["/commons-34f9cf73772c04929802.js","97bad37a5c828db0faa60769be7c8e83"],["/index.html","3fcefcfc37c60571445a5ed500191989"],["/manifest.json","2027904c18caa3d9f4569177e8406bea"],["/offline-plugin-app-shell-fallback/index.html","4cfbd611da57db33fbad4ec72c89f780"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
