b5fa71c82cd23a8b0772215b91aa9d10f3b50a82
Nick Cuthbert <nick@cuthbert.co.za>
2019-04-09 23:20:07 +0200

    Added language selector

--- src/components/Layout/LanguageDrawer.jsx
+++ src/components/Layout/LanguageDrawer.jsx
@@ -17,6 +17,7 @@ const LanguageDrawerDiv = styled.div`
   height: 100vh;
   z-index: 100000;
   padding: 25px;
+  animation-duration: 300ms;  
 `;
 
 const HeaderContainer = styled.div`
@@ -24,16 +25,16 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
   width: 100%;
   flex-wrap: wrap;
-  background: ${props => props.isSubpage ? props.theme.brand : 'inherit'};        
-  margin: ${props => props.isSubpage ? 'inherit' : '0 auto'};        
+  background: 'inherit';        
+  margin:  '0 auto';        
   .nav-link {
     font-size: 1.6rem;
     margin-right: 10px;
     font-weight: 200;           
-    color: ${props => props.isSubpage ? props.theme.accent : props.theme.brand};      
+    color: props.theme.brand;
   }  
   .nav-link:hover {
-    border-color:  ${props => props.isSubpage ? props.theme.accent : props.theme.brand};
+    border-color: props.theme.accent;
   }
 `;
 
@@ -43,8 +44,7 @@ const Hamburger = styled.section`
   }
   button {
     background: none;
-    border: none;
-    // color:  ${props => props.theme.accent}; 
+    border: none;    
     color:  ${props => props.theme.brand};
   }  
   
@@ -62,17 +62,53 @@ const Hamburger = styled.section`
     height: 2.5em;
   }
 
-  animation-delay: 350ms;
-  /*.main {
-    color:  ${props => props.theme.brand};
-  }*/
 
 `;
 
+const LanguageContainer = styled.div`
+  max-width: 66rem;
+  padding: 2rem;
+  margin: auto;
+
+  ul {
+    display: flex;
+    flex-direction: row;    
+    flex-wrap: wrap;    
+    justify-content: center;
+    width: 100%;
+    list-style-type: '';
+  }
+
+  h1, h2, p { 
+    color: ${props => props.theme.ink};
+    text-align: center;
+  }
+
+  li {
+    margin: 1rem;    
+  }
+`;
+
 class LanguageDrawer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { open: false, timeout: null };
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    if (prevProps.menuOpen && !this.props.menuOpen) {
+      const timeout = setTimeout(() => {
+        this.setState({ open: false, timeout: null });
+      }, 300);
+      this.setState({ timeout: timeout });
+    } else if (!prevProps.menuOpen && this.props.menuOpen) {
+      this.setState({ open: true });
+    }
+  }
+
   render () {
     return (
-      <LanguageDrawerDiv menuOpen={this.props.menuOpen}>
+      <LanguageDrawerDiv menuOpen={this.state.open} className={`animated ${this.props.menuOpen ? 'fadeIn' : 'fadeOut'}`}>
         <HeaderContainer>
           <section>
             {this.props.isSubpage
@@ -80,30 +116,32 @@ class LanguageDrawer extends React.Component {
             : <Link className='nav-link' to='/' ><img style={{ height: '1.5em' }} alt='logo' src='/logos/logo-wide-inverted.svg' /></Link>
           }
           </section>
-          <Hamburger className={this.props.isSubpage ? ' ' : 'animated fadeIn'}>
-            <button onClick={() => this.toggleMenu()} className={this.props.isSubpage ? 'subpage' : 'main'}>
+          <Hamburger>
+            <button onClick={() => this.props.closeDrawer()} className={'main'}>
               <span>✕</span>
             </button>
           </Hamburger>
         </HeaderContainer>
-        <h1>Languages</h1>
-        <div>Nact documentation is available in the following languages:</div>
-        <div>Nact 문서는 다음 언어로 제공됩니다.</div>
-        <LanguageSection>
-          <h2>JavaScript</h2>
-          <div>
+        <LanguageContainer>
+          <h1>Languages</h1>
+          <p>Nact documentation is available in the following languages<br />
+             Nact 문서는 다음 언어로 제공됩니다.</p>
+          <LanguageSection>
+            <h2><img alt='javascript docs' style={{ height: '1em' }} src='/logos/language-logo_js-inverted.svg' /> JavaScript</h2>
+            <div>
+              <ul>
+                <li><Link to='/en_uk/lesson/javascript/introduction'>English (en_uk)</Link></li>
+                <li><Link to='/ko_kr/lesson/javascript/nact-%EC%86%8C%EA%B0%9C'>한국어 (ko_kr)<sup>✸</sup></Link></li>
+              </ul>
+            </div>
+          </LanguageSection>
+          <LanguageSection>
+            <h2><img alt='reasonml docs' style={{ height: '1em' }} src='/logos/language-logo_reason-inverted.svg' /> ReasonML</h2>
             <ul>
-              <li>English (en_uk)</li>
-              <li>한국어 (ko_kr)</li>
+              <li><Link to='/en_uk/lesson/reasonml/introduction'>English (en_uk)</Link></li>
             </ul>
-          </div>
-        </LanguageSection>
-        <LanguageSection>
-          <h2>ReasonML</h2>
-          <ul>
-            <li>English (en_uk)</li>
-          </ul>
-        </LanguageSection>
+          </LanguageSection>
+        </LanguageContainer>
       </LanguageDrawerDiv>
     );
   }
--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -141,8 +141,12 @@ class Navigation extends React.Component {
     this.setState({ menuOpen: !this.state.languageMenuOpen && !this.state.menuOpen });
   }
 
-  toggleLanguageMenu () {
-    this.setState({ languageMenuOpen: !this.state.languageMenuOpen, menuOpen: false });
+  openLanguageDrawer () {
+    this.setState({ languageMenuOpen: true, menuOpen: false });
+  }
+
+  closeLanguageDrawer () {
+    this.setState({ languageMenuOpen: false });
   }
 
   render () {
@@ -165,18 +169,18 @@ class Navigation extends React.Component {
             <input placeholder='search docs' type='search' ref={() => this.mountDocsearch()} className='search-box' id='search-box' />
           </Search>}
           <div>
-            <a onClick={() => this.toggleLanguageMenu()} className='nav-link' href='#'>
+            <a onClick={() => this.openLanguageDrawer()} className='nav-link' href='#'>
               <svg style={{ height: '1em' }} width='24' height='24' viewBox='0 0 24 24'>
                 <path fill={this.props.isSubpage ? 'white' : '#FC5B5B'} d='M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z' />
               </svg>
-              LANGUAGE
+              LANGUAGE<sup>✸</sup>
             </a>
           </div>
           <div><Link className='nav-link' to='/blog' > BLOG </Link></div>
           <div><Link className='nav-link' to='/community' > COMMUNITY </Link></div>
           <div><a className='nav-link' href='https://github.com/ncthbrt/nact'> GITHUB </a></div>
         </NavLinks>
-        <LanguageDrawer menuOpen={this.state.languageMenuOpen} />
+        <LanguageDrawer closeDrawer={() => this.closeLanguageDrawer()} menuOpen={this.state.languageMenuOpen} />
       </NavContainer>
     );
   }
